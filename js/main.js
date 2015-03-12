6a730628c8e60d77cd2eaf3594082c889d9d76d3
Brad Doyle <BDProductionsStudios@gmail.com>
2015-03-12 17:50:06 -0400

    Mar 12
    Toggle working just not correctly need to start it hidden other then
    that everything is good.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 var $btnShowHide = $('.btn-show-hide');
 var $box = $('.box');
-var $btnShow = $('.js-box-show')
 var $hOne = $('.none');
 
 $btnShowHide.on('click', function () {
    $box.toggleClass('js-box-show-hide');
-
+   $hOne.removeClass('none');
+   sessionStorage.setItem('active','yes');
 });
 
-$btnShow.on('click', function () {
-  $hOne.removeClass('none')
+if(sessionStorage.getItem('active')){
+  $box.toggleClass('js-box-show-hide');
+  $hOne.removeClass('none');
+}
+
+$(document).ready(function(){
+  $('.toggle').click(function(){
+    $('.menu').slideToggle(700);
+    $('.menu').toggle(display);
+  });
 });
 
+if ( display === false ) {
+  $('.menu').show();
+} else if ( display === false ) {
+  $('.menu').hide();
+}
