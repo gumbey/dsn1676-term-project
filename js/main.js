5a4387b22e7e04fa816cdd1f61ecfe614e4a29c3
Brad Doyle <BDProductionsStudios@gmail.com>
2015-04-16 20:22:10 -0400

    js rearranged

--- js/main.js
+++ js/main.js
@@ -78,3 +78,13 @@ $('.top-nav').localScroll();
 $('.nav').localScroll();
 $('.midnav').localScroll();
 
+var $bitme = $('.bitme')
+var $head = $('.head')
+
+$head.waypoint(function (direction) {
+  if (direction == 'down') {
+    $head.removeClass('headlook');
+  } else {
+    $head.addClass('headlook');
+  }
+}, {offset: '10%'});
